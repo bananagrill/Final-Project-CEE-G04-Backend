9307642ac5f98d6a2de3e034c07d3e321474d639
root <root@ip-172-31-86-90.ec2.internal>
2023-04-25 14:27:06 +0000

    add

---
+++ controller/dateTime.js
@@ -0,0 +1,14 @@
+function getTime() {
+  const now = new Date();
+  const year = now.getFullYear().toString();
+  const month = (now.getMonth() + 1).toString().padStart(2, "0");
+  const day = now.getDate().toString().padStart(2, "0");
+  const hour = now.getHours().toString().padStart(2, "0");
+  const minute = now.getMinutes().toString().padStart(2, "0");
+  const second = now.getSeconds().toString().padStart(2, "0");
+  return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+}
+
+module.exports = {
+  getTime: getTime,
+};
