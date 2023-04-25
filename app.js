22e8e5921ba395096d34a6aca96008f0fe426e95
root <root@ip-172-31-86-90.ec2.internal>
2023-04-25 14:59:49 +0000

    edit app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/post", postRoutes);
 app.use("/courseville", coursevilleRoutes);
 app.get("/", (req, res) => {
-  res.send("ไอ่ไนท์ไอ่โง่");
+  res.send("Server run correctly.");
 });
 
 app.all("*", (req, res, next) => {
